0d3252d2965e1273af2a1344dabfcd527cbf9a54
PRABHANSH TIWARI <140301155+prabhanshtiwari@users.noreply.github.com>
2023-12-02 20:58:47 +0530

    Create Program3.cube_volume.py
    A python Program to calculate and display the volume of a CUBE having its height (h=10cm), width (w=12cm) and depth (8cm).

---
+++ Program3.cube_volume.py
@@ -0,0 +1,12 @@
+# Write a python Program to calculate and display the volume of a CUBE having its height (h=10cm), width (w=12cm) and depth (8cm). 
+
+# Given values
+height = 10  # in centimeters
+width = 12   # in centimeters
+depth = 8    # in centimeters
+
+# Calculating the volume of the cube
+volume = height * width * depth
+
+# Displaying the result
+print(f"The volume of the cube is: {volume} cubic centimeters")
