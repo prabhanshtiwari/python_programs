24a23a76f92a5b376b2ddcc100cae17b56786d3a
PRABHANSH TIWARI <140301155+prabhanshtiwari@users.noreply.github.com>
2023-12-02 21:15:38 +0530

    Update Program4.area_circumference_circle.py
    A python program to calculate area and circumference of a circle.

--- Program4.area_circumference_circle.py
+++ Program4.area_circumference_circle.py
@@ -1,9 +1,9 @@
 # Write a python program to calculate area and circumference of a circle. 
 
-radius = float(int(input("Enter the radius of circle: "))
+radius = float(input("Enter the radius of circle: "))
 PI = 3.1415
 area_of_circle = PI * radius ** 2
 circumference_of_circle = 2 * PI * radius
 
-print(f"Area of circle of radius = {radius) unit is {area_of_circle} square unit")
-print(f"Circumference of circle of radius = {radius) unit is {circumference_of_circle} unit")
+print(f"Area of circle of radius = {radius} unit is {area_of_circle} square unit")
+print(f"Circumference of circle of radius = {radius} unit is {circumference_of_circle} unit")
