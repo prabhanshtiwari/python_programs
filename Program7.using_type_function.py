57d1a9b4176fd7e0919260272ceab96ad6c74767
PRABHANSH TIWARI <140301155+prabhanshtiwari@users.noreply.github.com>
2023-12-03 07:35:23 +0530

    Create Program7.using_type_function.py
    A python program for assigning the different date type value in a variable and display the data type name using type().

---
+++ Program7.using_type_function.py
@@ -0,0 +1,16 @@
+# Write the python program for assigning the different date type value in a variable and display the data type name using type().
+
+variable = 108
+print(type(variable))   # <class 'int'>
+
+variable = 108.80
+print(type(variable))   # <class 'float'>
+
+variable = 3 + 2j
+print(type(variable))   # <class 'complex'>
+
+variable = True
+print(type(variable))   # <class 'bool'>
+
+variable = "We are learning Python"
+print(type(variable))   # <class 'str'>
