8dbb2f857b147debbca0c13bad79882af8293586
PRABHANSH TIWARI <140301155+prabhanshtiwari@users.noreply.github.com>
2023-12-03 07:23:36 +0530

    Create Program6.solving_equations.py

---
+++ Program6.solving_equations.py
@@ -0,0 +1,18 @@
+# Equation a: (x+y)*(x-y) where x=10 & y=20
+x_a = 10
+y_a = 20
+result_a = (x_a + y_a) * (x_a - y_a)
+print("Result of equation a:", result_a)
+
+# Equation b: (a+b)**2 where a=5 & b=3
+a_b = 5
+b_b = 3
+result_b = (a_b + b_b) ** 2
+print("Result of equation b:", result_b)
+
+# Equation c: (x+y+z)**5 where x=3, y=5 & z=7
+x_c = 3
+y_c = 5
+z_c = 7
+result_c = (x_c + y_c + z_c) ** 5
+print("Result of equation c:", result_c)
