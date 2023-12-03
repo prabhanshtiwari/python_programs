7c4a16d622c0cbc03554857516090311da311f85
PRABHANSH TIWARI <140301155+prabhanshtiwari@users.noreply.github.com>
2023-12-03 07:15:22 +0530

    Create Program5-sum_average.py
    A python program to display sum & average of 10,20,30,40,50 values.

---
+++ Program5-sum_average.py
@@ -0,0 +1,16 @@
+# Write a python program to display sum & average of 10,20,30,40,50 values.
+
+# Given inputs
+number1 = 10
+number2 = 20
+number3 = 30
+number4 = 40
+number5 = 50
+
+# Calculating sum and average
+sum = number1 + number2 + number3 + number4 + number5
+average = sum / 5
+
+# Displaying sum and average
+print("Sum = ", sum)
+print("Average = ", average)
