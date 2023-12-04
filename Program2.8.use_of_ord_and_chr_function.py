758cf756f046cc282fdea2478edfd70e66e17f2e
PRABHANSH TIWARI <140301155+prabhanshtiwari@users.noreply.github.com>
2023-12-04 19:37:51 +0530

    Create Program2.8.use_of_ord_and_chr_function.py
    A program to demonstrate the use of ord() and chr().

---
+++ Program2.8.use_of_ord_and_chr_function.py
@@ -0,0 +1,19 @@
+# Write a program to demonstrate the use of ord() and chr().
+
+print("Using ord() --> to get ASCII values")
+print("ASCII value of Lowercase letters")
+print("a =", ord("a")) 
+print("b =", ord("b"))
+
+print("ASCII value of Uppercase letters")
+print("A =", ord("A"))
+print("B =", ord("B"))
+
+print("ASCII value of Special symbols")
+print("! =", ord("!"))
+print("@ =", ord("@"))
+
+
+print("Using chr() --> to get character of the given ASCII values")
+print(97, "=", chr(97))
+print(98, "=", chr(98))
